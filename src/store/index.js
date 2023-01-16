806658b11b43aa3f6b30625d12d22e0ef17def79
soran <soran.taak@gmail.com>
2023-01-16 20:36:29 +0330

    add persist redux to Todo project

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
 import todosReducer from "./slices/todosSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+const rootReducer = combineReducers({
+	todos: todosReducer,
+});
+const persistConfig = {
+	key: "root",
+	storage,
+	witheList: ["todos"],
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-	reducer: {
-		todos: todosReducer,
-	},
+	reducer: persistedReducer,
 });
+const persistor = persistStore(store);
 
-export default store;
+export { store, persistor };
